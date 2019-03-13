19e7a95e2ef197f8d4fe7be8b51e4c9845aa6709
VanessaCalimag <vanessa.calimag@my.jcu.edu.au>
2019-03-13 15:04:04 +1000

    Refactor temperatures, sales bonus and broken score programs to use functions

--- prac_03/sales_bonus.py
+++ prac_03/sales_bonus.py
@@ -1,14 +1,26 @@
+"""
+Program to calculate and display a user's bonus based on sales.
+If sales are under $1,000, the user gets a 10% bonus.
+If sales are $1,000 or over, the bonus is 15%.
+"""
 
 
-sales = float(input("Enter sales: $"))
-while sales != 0:
-    if sales < 1000:
-        bonus = sales * .1
-        print("Bonus: ${:.2f}".format(bonus))
-    elif sales >= 1000:
-        bonus = sales * .15
-        print("Bonus: ${:.2f}".format(bonus))
-    else:
-        print("Invalid amount")
-        sales = float(input("Enter sales: $"))
+def main():
+    sales = float(input("Enter sales: $"))
+    print(compute_bonus(sales))
     print("Thank you.")
+
+
+def compute_bonus(sales):
+    while sales != 0:
+        if sales < 1000:
+            bonus = sales * .1
+            return "Bonus: ${:.2f}".format(bonus)
+        elif sales >= 1000:
+            bonus = sales * .15
+            return "Bonus: ${:.2f}".format(bonus)
+        else:
+            return "Invalid amount"
+
+
+main()
