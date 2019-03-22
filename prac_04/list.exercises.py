a4d52cf8ac50fc77391749e056fd34effc836c2d
jc448882 <vanessa.calimag@my.jcu.edu.au>
2019-03-22 18:11:06 +1000

    Add several programs using list comprehension

--- prac_04/list.exercises.py
+++ prac_04/list.exercises.py
@@ -1,15 +1,17 @@
 
-
-# numbers = []
-# number = int(input('Number: '.format(numbers)))
+""""""
+numbers = []
+for i in range(5):
+    number = int(input('Number: '.format(numbers)))
+    numbers.append(number)
 # while len(numbers) < 5:
 #     numbers.append(number)
 #     number = int(input('Number: '.format(numbers)))
-# print('The first number is {}'. format(numbers[0]))
-# print('The last number is {}'.format(numbers[-1]))  # figure this out
-# print('The smallest number is', min(numbers))
-# print('The largest number is', max(numbers))
-# print('The average of the numbers is', sum(numbers) / len(numbers))
+print('The first number is {}'. format(numbers[0]))
+print('The last number is {}'.format(numbers[-1]))  # figure this out
+print('The smallest number is', min(numbers))
+print('The largest number is', max(numbers))
+print('The average of the numbers is', sum(numbers) / len(numbers))
 
 
 usernames = ['jimbo', 'giltson98', 'derekf', 'WhatSup', 'NicolEye', 'swei45',
