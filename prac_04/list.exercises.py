d8698a164a05c0ed5bba32f3bd37e17e958e5fdb
jc448882 <vanessa.calimag@my.jcu.edu.au>
2019-03-22 18:02:40 +1000

    Add program which performs username verification

--- prac_04/list.exercises.py
+++ prac_04/list.exercises.py
@@ -1,15 +1,24 @@
 
-def main():
-    numbers = []
-    number = int(input('Number: '.format(numbers)))
-    while len(numbers) < 5:
-        numbers.append(number)
-        number = int(input('Number: '.format(numbers)))
-    print('The first number is {}'. format(numbers[0]))
-    print('The last number is {}'.format(numbers[-1]))  # figure this out
-    print('The smallest number is', min(numbers))
-    print('The largest number is', max(numbers))
-    print('The average of the numbers is', sum(numbers) / len(numbers))
+
+# numbers = []
+# number = int(input('Number: '.format(numbers)))
+# while len(numbers) < 5:
+#     numbers.append(number)
+#     number = int(input('Number: '.format(numbers)))
+# print('The first number is {}'. format(numbers[0]))
+# print('The last number is {}'.format(numbers[-1]))  # figure this out
+# print('The smallest number is', min(numbers))
+# print('The largest number is', max(numbers))
+# print('The average of the numbers is', sum(numbers) / len(numbers))
 
 
-main()
+usernames = ['jimbo', 'giltson98', 'derekf', 'WhatSup', 'NicolEye', 'swei45',
+             'BaseInterpreterInterface', 'BaseStdIn', 'Command', 'ExecState',
+             'InteractiveConsole', 'InterpreterInterface', 'StartServer', 'bob']
+username = input('Username: ')
+if username in usernames:
+    print("Access granted")
+else:
+    print("Access denied")
+    username = input('Username: ')
+
