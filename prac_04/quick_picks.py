22ed646370f5b144ea148b2e150aaa661327d350
jc448882 <vanessa.calimag@my.jcu.edu.au>
2019-03-23 10:54:05 +1000

    Edit quick pick codes to print the number of lines given by the user.

--- prac_04/quick_picks.py
+++ prac_04/quick_picks.py
@@ -6,10 +6,23 @@ NUMBER_LINES = 6
 MIN = 1
 MAX = 45
 
-quick_pick = []
-number_quick_picks = int(input('How many quick picks? '))
-for i in range(number_quick_picks):
-    number = random.randint(MIN, MAX)
-    quick_pick.append(number)
 
-print(quick_pick)
+def main():
+    number_quick_picks = int(input('How many quick picks? '))
+    while number_quick_picks < 0:
+        print('Invalid input. Try again.')
+        number_quick_picks = int(input('How many quick picks? '))
+
+    for i in range(number_quick_picks):
+        quick_pick = []
+        for j in range(NUMBER_LINES):
+            number = random.randint(MIN, MAX)
+            while number in quick_pick:
+                number = random.randint(MIN, MAX)
+            quick_pick.append(number)
+            quick_pick.sort()
+        # print(quick_pick)
+        print(" ".join("{:6}".format(number) for number in quick_pick))
+
+
+main()
