24b0ba4f8afec7728aa2737c1ed8f4e106361c39
jc448882 <vanessa.calimag@my.jcu.edu.au>
2019-03-27 14:10:24 +1000

    Edit word occurrence program

--- prac_05/word.occurrences.py
+++ prac_05/word.occurrences.py
@@ -2,6 +2,15 @@
 
 text = {}
 string_of_words = input('Text: ')
+# This is a collection of words of nice words this is a fun thing it is
 
+# Splitting words from user's input
+words = string_of_words.split()
+for word in words:
+    occurrence = text.get(word, 0) + 1
+    text[word] = occurrence
+    words.sort()
+    print(word)
 
-
+word_length = (len(word)for word in words)
+print('{} : {}'.format(word, word_length))
