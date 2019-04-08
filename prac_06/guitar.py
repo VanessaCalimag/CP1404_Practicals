319f74c1ba5db2171fb34587eb3a8c065c117cf4
VanessaCalimag <vanessa.calimag@my.jcu.edu.au>
2019-04-08 22:53:52 +1000

    Add guitar program

---
+++ prac_06/guitar.py
@@ -0,0 +1,21 @@
+"""Guitar class"""
+
+
+class Guitar:
+    CURRENT_YEAR = 2019
+
+    def __init__(self, name='', year=0, cost=0):
+        self.name = name
+        self.year = year
+        self.cost = cost
+
+    def __str__(self):
+        return '{self.name} ({self.year}) : ${self.cost:.2f}'.format(self=self)
+
+    def get_age(self):
+        return Guitar.CURRENT_YEAR - self.year
+
+    def is_vintage(self):
+        return Guitar.get_age >= 50
+
+
