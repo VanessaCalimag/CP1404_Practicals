423ec384467b8ffcb432d73173d88444c036ebf6
VanessaCalimag <vanessa.calimag@my.jcu.edu.au>
2019-04-29 19:15:05 +1000

    Create distance converter program

---
+++ prac_07/distance_converter.py
@@ -0,0 +1,22 @@
+from kivy.app import App
+from kivy.lang import Builder
+from kivy.app import StringProperty
+
+
+class DistanceConverter(App):
+    message = StringProperty()
+
+    def build(self):
+        self.title = 'Convert Miles to Kilometres'
+        self.root = Builder.load_file('distance_converter.kv')
+        return self.root
+
+    def handle_calculate(self, value):
+        result = value * 1.60934
+        self.root.ids.output_label.text = str(result)
+
+    def handle_press(self):
+        self.message = self.root.ids.input_number.text
+
+
+DistanceConverter().run()
